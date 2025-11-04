1e9885658ce8a7681aecdacca8ff169438b99c81
geeksamarth <samarthagrawal1804@gmail.com>
2025-11-04 17:58:04 +0530

    add: routes and controller for login and fetching the current loggedIn user

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,8 +1,12 @@
 import { db } from "../db/index.js";
 import { usersTable } from "../models/index.js";
-import { signupostRequestBodySchema } from "../validations/request.validations.js";
+import {
+  signupostRequestBodySchema,
+  loginpostRequestBodySchema,
+} from "../validations/request.validations.js";
 import { hashedPasswordWithSalt } from "../utils/hashing.js";
 import { getUserByEmail } from "../services/user.services.js";
+import { createUserToken } from "../utils/token.js";
 
 export const signup = async (req, res) => {
   const validationResult = await signupostRequestBodySchema.safeParseAsync(
@@ -37,4 +41,29 @@ export const signup = async (req, res) => {
     })
     .returning({ id: usersTable.id });
   return res.status(201).json({ data: { userID: user.id } });
+};
+
+export const login = async (req, res) => {
+  const validationResult = await loginpostRequestBodySchema.safeParseAsync(
+    req.body
+  );
+  if (validationResult.error) {
+    return res.status(400).json({ error: validationResult.error.format() });
+  }
+  const { email, password } = validationResult.data;
+  const user = await getUserByEmail(email);
+  if (!user) {
+    return res.status(404).json({
+      error: `User with email ${email} does not exists, please register first!`,
+    });
+  }
+  const { password: hashedPassword } = hashedPasswordWithSalt(
+    password,
+    user.salt
+  );
+  if (user.password !== hashedPassword) {
+    return res.status(400).json({ error: `Invalid Password ` });
+  }
+  const token = await createUserToken({ id: user.id });
+  return res.json({ token });
 };
