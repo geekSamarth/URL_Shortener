04562d8ba8448f696c3ba7f2bae672961ef874d2
geeksamarth <samarthagrawal1804@gmail.com>
2025-11-07 20:09:44 +0530

    add: cors configuration to solve the issue of cors error

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 import express from "express";
+import cors from "cors";
 import userRouter from "./routes/user.routes.js";
 import urlRouter from "./routes/url.routes.js";
 import { authenticationMiddleware } from "./middlewares/auth.middlewares.js";
 const app = express();
 const PORT = 8000;
 
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    allowedHeaders: ["Authorization", "Content-type"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  })
+);
 app.use(express.json());
 app.use(authenticationMiddleware);
 
 app.get("/", (req, res) => {
   return res.json({ status: "Server is up and running ..." });
 });
-app.use(urlRouter);
 app.use("/user", userRouter);
+app.use(urlRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT $${PORT}`);
