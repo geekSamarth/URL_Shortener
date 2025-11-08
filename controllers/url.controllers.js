eb8f1fa4210258f7f97c7c174d139874162f964f
geeksamarth <samarthagrawal1804@gmail.com>
2025-11-08 17:27:45 +0530

    add: controllers and routes for URLs

--- controllers/url.controllers.js
+++ controllers/url.controllers.js
@@ -2,6 +2,7 @@ import { shortenPostRequestBodySchema } from "../validations/request.validations
 import { nanoid } from "nanoid";
 import { db } from "../db/index.js";
 import { urlsTable } from "../models/url.models.js";
+import { and, eq } from "drizzle-orm";
 
 export const shortenUrl = async (req, res) => {
   const validationResult = await shortenPostRequestBodySchema.safeParseAsync(
@@ -29,4 +30,35 @@ export const shortenUrl = async (req, res) => {
     shortCode: result.shortCode,
     targetURL: result.targetURL,
   });
+};
+
+export const redirectOriginalURL = async (req, res) => {
+  const shortCode = req.params.shortcode;
+  console.log("shortcode", shortCode);
+
+  const [result] = await db
+    .select()
+    .from(urlsTable)
+    .where(eq(urlsTable.shortCode, shortCode));
+  if (!result) {
+    return res.status(404).json({ error: `Invalid URL` });
+  }
+  return res.redirect(result.targetURL);
+};
+
+export const getAllShortenedURLs = async (req, res) => {
+  const codes = await db
+    .select()
+    .from(urlsTable)
+    .where(eq(urlsTable.userId, req.user.id));
+  return res.json({ codes });
+};
+
+export const deleteURL = async (req, res) => {
+  const id = req.params.id;
+
+  await db
+    .delete(urlsTable)
+    .where(and(eq(urlsTable.id, id)), eq(urlsTable.userId, req.user.id));
+  return res.status(200).json({ deleted: true });
 };
--- routes/url.routes.js
+++ routes/url.routes.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { ensureAuthenticated } from "../middlewares/auth.middlewares";
+import { ensureAuthenticated } from "../middlewares/auth.middlewares.js";
+import {
+  deleteURL,
+  getAllShortenedURLs,
+  redirectOriginalURL,
+  shortenUrl,
+} from "../controllers/url.controllers.js";
 const router = express.Router();
 
 // routes for URL's
 
 router.post("/shorten", ensureAuthenticated, shortenUrl);
+router.get("/all-urls", ensureAuthenticated, getAllShortenedURLs);
+router.delete("/urls/:id", ensureAuthenticated, deleteURL);
+router.get("/:shortcode", redirectOriginalURL);
 
 export default router;
