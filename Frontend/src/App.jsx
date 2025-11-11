03622019d32d724d31678d0028f713d7393d51cf
geeksamarth <samarthagrawal1804@gmail.com>
2025-11-11 18:42:58 +0530

    add: setting up the basic folder structure for the development

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='bg-black text-yellow-600'>URL Shortener Frontend Development</h1>
+    </>
+  )
+}
+
+export default App
